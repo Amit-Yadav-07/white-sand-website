802182e5a4864019da9294f658d0f3245d97a89a
Amit <amitsinghyadav974@gmail.com>
2024-08-16 15:21:10 +0530

    more feature added

---
+++ data.js
@@ -0,0 +1,36 @@
+export let reviews = [
+
+    {
+        id: 1,
+        name: 'Anna Johnson',
+        designation: 'Programmer',
+        img: 'https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947_1280.jpg',
+        about: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+
+    },
+
+    {
+        id: 2,
+        name: 'Peter Jones',
+        designation: 'UI-UX designer',
+        img: 'https://cdn.pixabay.com/photo/2017/12/17/08/12/girl-3023831_1280.jpg',
+        about: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal."
+    },
+
+    {
+        id: 3,
+        name: 'Bill Anderson',
+        designation: 'The Boss',
+        img: 'https://cdn.pixabay.com/photo/2015/07/20/13/01/man-852770_1280.jpg',
+        about: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag."
+    },
+
+    {
+        id: 4,
+        name: 'Susan Smith',
+        designation: 'Developer',
+        img: 'https://cdn.pixabay.com/photo/2016/11/18/14/08/jetty-1834801_1280.jpg',
+        about: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic."
+    },
+];
+
--- index.js
+++ index.js
@@ -1,3 +1,52 @@
-const Toggle = document.querySelector('.fa-solid');
-console.log(Toggle);
-// Toggle.classList.toggle(' fa-xmark')
+import { reviews } from "./data.js";
+
+const user_img = document.getElementById('user-img');
+const user_name = document.getElementById('user-name');
+const user_designation = document.getElementById('user-designation');
+const about_user = document.getElementById('about-user');
+const leftBtn = document.querySelector('.fa-angle-left');
+const rightBtn = document.querySelector('.fa-angle-right');
+
+let initialValue = 0;
+
+window.addEventListener('DOMContentLoaded', function () {
+
+    CurrentUser(initialValue)
+})
+
+
+rightBtn.addEventListener('click', function () {
+    initialValue++;
+    if (initialValue > reviews.length - 1) {
+        initialValue = 0;
+
+    }
+    CurrentUser(initialValue);
+})
+
+leftBtn.addEventListener('click', function () {
+    initialValue--;
+    if (initialValue < 0) {
+        initialValue = reviews.length - 1
+    }
+    CurrentUser(initialValue);
+})
+
+
+function CurrentUser(person) {
+    let item = reviews[person];
+    user_img.src = item.img;
+    user_name.textContent = item.name;
+    user_designation.textContent = item.designation;
+    about_user.textContent = item.about;
+}
+
+
+setInterval(() => {
+
+    CurrentUser(initialValue++);
+    if (initialValue > reviews.length - 1) {
+        initialValue = 0;
+    }
+
+}, 3000)
