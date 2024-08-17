ca73e86e5904e09f86aad2bcd8e7e56133d14e89
Amit <amitsinghyadav974@gmail.com>
2024-08-18 01:07:42 +0530

    slick-autoplay

--- about.js
+++ about.js
@@ -1,3 +1,40 @@
 
 
-console.log('hey');
+$('.our-team-parent').slick({
+    dots: true,
+    autoplaySpeed: 3000,
+    // centerMode: true,
+    // centerPadding: '80px',
+    slidesToScroll: 1,
+    autoplay: true,
+    infinite: true,
+    speed: 100,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
