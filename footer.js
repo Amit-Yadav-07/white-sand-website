8294de3aa93ef661f63617aa7742a1756497398b
Amit <amitsinghyadav974@gmail.com>
2024-10-01 18:01:28 +0530

    footer-img-added

--- footer.js
+++ footer.js
@@ -47,39 +47,39 @@ export const footer = `<div class="footer-container">
                                         <div class="row g-3">
                                             <div class="col-4">
                                                 <div class="footer-instagram rounded">
-                                                    <img src="./images/8.png" class="img-fluid w-100"
+                                                    <img src="./images/footer-img-2.png" class="img-fluid w-100"
                                                         alt="">
                                                 </div>
                                             </div>
                                             <div class="col-4">
                                                 <div class="footer-instagram rounded">
-                                                    <img src="./images/8.png" class="img-fluid w-100"
+                                                    <img src="./images/footer-img-6.png" class="img-fluid w-100"
                                                         alt="">
                                                 </div>
                                             </div>
                                             <div class="col-4">
                                                 <div class="footer-instagram rounded">
-                                                    <img src="./images/8.png" class="img-fluid w-100"
+                                                    <img src="./images/footer-img-5.png" class="img-fluid w-100"
                                                         alt="">
                                                 </div>
                                             </div>
                                             <div class="col-4">
                                                 <div class="footer-instagram rounded">
-                                                    <img src="./images/8.png" class="img-fluid w-100"
-                                                        alt="">
-                                                    
-                                                </div>
-                                            </div>
-                                            <div class="col-4">
-                                                <div class="footer-instagram rounded">
-                                                    <img src="./images/8.png" class="img-fluid w-100"
+                                                    <img src="./images/footer-img-1.png" class="img-fluid w-100"
                                                         alt="">
                                                     
                                                 </div>
                                             </div>
                                             <div class="col-4">
                                                 <div class="footer-instagram rounded">
-                                                    <img src="./images/8.png" class="img-fluid w-100"
+                                                    <img src="./images/footer-img-4.png" class="img-fluid w-100"
+                                                        alt="">
+                                                    
+                                                </div>
+                                            </div>
+                                            <div class="col-4">
+                                                <div class="footer-instagram rounded">
+                                                    <img src="./images/footer-img-3.png" class="img-fluid w-100"
                                                         alt="">
                                                 </div>
                                             </div>
